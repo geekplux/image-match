e37514a9f9e3c424ac15b29dff7ca50c0f8c6d73
geekplux <geekplux@qq.com>
2016-06-06 20:36:12 +0800

    calculate gray

--- index.js
+++ index.js
@@ -13,30 +13,71 @@
   var imgMatch = {};
 
   imgMatch.compare = function (img1, img2) {
-    console.log(getImageData(img1));
+    var imgData = getImgData(img1);
+    console.log(imgData);
 
     var similarity = 1;
     return similarity;
   };
 
+
+  /**
+   *
+   * @param {String} src image url
+   * @returns {Object}
+   */
   function getImageData(src) {
     var canvas = drawImage(src);
     var ctx = canvas.getContext('2d');
-    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    return imageData;
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return imgData;
   }
 
+
+  /**
+   *
+   * @param {String} src image url
+   * @returns {Object} canvas object
+   */
   function drawImage (src) {
-    var image = new Image();
+    var img = new Image();
     var canvas = document.createElement('canvas');
-    image.onload = function() {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      canvas.getContext('2d').drawImage(image, 0, 0);
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.getContext('2d').drawImage(img, 0, 0);
     };
 
-    image.src = src;
+    img.src = src;
     return canvas;
+  }
+
+
+  /**
+   * refactor the image data to a RGB array.
+   * @param {Object} imgData canvas getImageData's result
+   * @returns {Array} a RGB array
+   */
+  function refactorRGB (imgData) {
+    var data = imgData.data;
+    var width = imgData.width;
+    var height = imgData.height;
+    var rgbArr = [];
+    for (h = 0; h < height; h += 12) {
+      for (w = 0; w < width; w += 6) {
+        var index = (w + width * h) * 4;
+        var r = imgDataArr[index + 0];
+        var g = imgDataArr[index + 1];
+        var b = imgDataArr[index + 2];
+        rgbArr.push({r: r, g: g, b: b});
+      }
+    }
+    return rgbArr;
+  }
+
+
+  function calculateGray(r, g, b) {
+    return 0.299 * r + 0.578 * g + 0.114 * b;
   }
 
   return imgMatch;
