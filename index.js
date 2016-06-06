4e9ee1149356a597d001f1c5b46b19c9e39a8ea2
geekplux <geekplux@qq.com>
2016-06-06 10:26:56 +0800

    add drawImage method and get image data

--- index.js
+++ index.js
@@ -13,9 +13,31 @@
   var imgMatch = {};
 
   imgMatch.compare = function (img1, img2) {
+    console.log(getImageData(img1));
 
+    var similarity = 1;
     return similarity;
   };
+
+  function getImageData(src) {
+    var canvas = drawImage(src);
+    var ctx = canvas.getContext('2d');
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return imageData;
+  }
+
+  function drawImage (src) {
+    var image = new Image();
+    var canvas = document.createElement('canvas');
+    image.onload = function() {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      canvas.getContext('2d').drawImage(image, 0, 0);
+    };
+
+    image.src = src;
+    return canvas;
+  }
 
   return imgMatch;
 });
